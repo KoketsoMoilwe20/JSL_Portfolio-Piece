62fdd8ca4aec58117fba836254aecc7068bc7ed5
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-04-25 10:12:43 +0200

    display stored tasks

--- index.js
+++ index.js
@@ -341,9 +341,27 @@ function saveTaskChanges(taskId) {
   // Close the modal and refresh the UI to reflect the changes
   location.reload();
   toggleModal(false, elements.editTaskModal);
-  
+
   refreshTasksUI();
 }
+
+const displayStoredTasks = () => {
+  // Retrieving the tasks from localStorage
+  const storedTasks = localStorage.getItem('tasks');
+
+  if (storedTasks) {
+    // Parsing the JSON string to an array of tasks
+    const tasks = JSON.parse(storedTasks);
+
+    // Loging the tasks to the console
+    console.log(tasks);
+  } else {
+    console.log('No tasks stored in localStorage.');
+  }
+}
+
+// Calling the function to display the stored tasks
+displayStoredTasks();
 
 /*************************************************************************************************************************************************/
 
